9406c48ce6032d2385398f2a8fc6b8bbe7a84253
zakaria abdessamed brahimi <brahimi.zakaria.abdessamed@gmail.com>
2020-09-17 14:00:50 +0100

    Sending email with attachments

--- Scripts/src/app/views.py
+++ Scripts/src/app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from django.core.mail import send_mass_mail, send_mail
-from email_sender.settings import EMAIL_HOST_USER
+from django.core.mail import send_mass_mail, send_mail,EmailMessage
+from email_sender.settings import EMAIL_HOST_USER,EMAIL_BACKEND
 from django.contrib import messages
 
 # Create your views here.
@@ -10,17 +10,33 @@ def send_email(request):
         subject        = request.POST['subject']
         recipient_list = request.POST['to_email']
         message        = request.POST['message']
-        send_mail(subject,
-                  message,
-                  EMAIL_HOST_USER,
-                  [recipient_list],
-                  fail_silently=False)
-        messages.success(request, f'email sent to {recipient_list} successfully.')
+        email = EmailMessage(subject,message,EMAIL_HOST_USER, [recipient_list])
+        # word = open("TO DO.docx", 'r')
+        email.attach('https://drive.google.com/file/d/1tNYrastT3PA6Lxd8-u_KDOSG7tkGof0r/view?usp=sharing', 'joiddsc.png')
+        # email.attach_file("Users\Zakaria Abdessamed\Desktop\ZakariaAbdessamedBrahimiResume.pdf", 'application/pdf')
+        email.send(fail_silently=False)
         return redirect('/')
-    else:
-        return render(request, 'index.html')
-
-    context = {
-        'recipient_list': recipient_list,
-        }
+    context = {}
     return render(request, 'index.html', context)
+
+
+# def send_email(request):
+#     if request.method == 'POST':
+#         
+#         subject        = request.POST['subject']
+#         recipient_list = request.POST['to_email']
+#         message        = request.POST['message']
+#         send_mass_mail((subject,
+#                   message,
+#                   EMAIL_HOST_USER,
+#                   [recipient_list]),
+#                   fail_silently=False)
+#         messages.success(request, f'email sent to {recipient_list} successfully.')
+#         # return redirect('/')
+#     else:
+#         return render(request, 'index.html')
+
+#     context = {
+#         'recipient_list': recipient_list,
+#         }
+#     return render(request, 'index.html', context)
